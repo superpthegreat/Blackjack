1a54f0d0a6fecb17b3aade09ce3a35e6db26806b
superpthegreat <82829436+superpthegreat@users.noreply.github.com>
2023-05-02 10:55:59 -0700

    Delete blackjack.py

--- blackjack.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-"""This module is a blackjack game module that runs the game"""
-
-from blackjack import game
-
-if __name__ == "__main__":
-    MAINGAME = game.BlackJackGame()
-    MAINGAME.run()
