f0c85081960463c84a71fab88744b6c5110c9c99
superpthegreat <82829436+superpthegreat@users.noreply.github.com>
2023-05-02 10:52:20 -0700

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -1,14 +1,4 @@
 #!/usr/bin/env python3
-# Anthony Soriano
-# CSPC 386-03
-# 2022-4-2
-# soriano_anthony473@csu.fullerton.edu
-# @anthony10154
-#
-# Lab 03-01
-#
-# This is my blackjack game program
-#
 
 """This module is a blackjack game module that runs the game"""
 
