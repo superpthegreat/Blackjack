b0d19726cca779f77fb7280e5dabae38d2b2f118
superpthegreat <82829436+superpthegreat@users.noreply.github.com>
2023-05-02 10:51:12 -0700

    Update cards.py

--- blackjack/cards.py
+++ blackjack/cards.py
@@ -1,14 +1,5 @@
 #!/usr/bin/env python3
-# Anthony Soriano
-# CSPC 386-03
-# 2022-4-2
-# soriano_anthony473@csu.fullerton.edu
-# @anthony10154
-#
-# Lab 03-01
-#
-# This is my blackjack game program
-#
+
 """This module is used to hold information about cards"""
 
 from collections import namedtuple
